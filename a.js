bd24da08ffbab4e598deaa524f6d2bc1d2d661f9
No-Blip <mebloquearonlacuenta@gmail.com>
2026-03-27 15:47:42 -0400

    actualizacionsssssss

--- a.js
+++ a.js
@@ -1,4 +1,4 @@
-fetch('/flag.txt')
+fetch('/')
   .then(response => response.text())
   .then(data => alert("Contenido de flag.txt: " + data))
   .catch(err => alert("Error al leer el archivo (posiblemente bloqueado por CSP o no existe)"));
